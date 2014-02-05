20afff155bd4f56dd40c4ebe11c4969e92951476
Daniel Jebaraj <danielj@syncfusion.com>
2014-02-05 02:59:44 -0500

    Updated Redis server IP

--- utils/src/main/java/com/syncfusion/utils/Configuration.java
+++ utils/src/main/java/com/syncfusion/utils/Configuration.java
@@ -4,6 +4,6 @@ package com.syncfusion.utils;
 public class Configuration {
     public static final int MAXVALUES = 10;
     public static final String REDIS_CACHE_KEY = "MAXTRACKER";
-    public static final String REDIS_SERVER = "192.168.56.102";
+    public static final String REDIS_SERVER = "127.0.0.1";
     public static final String SENSOR_NAME = "SensorZ";
 }
